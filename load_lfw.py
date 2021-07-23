e3d36a223b11828c2530d502046c253f4e7c6f12
oStritze <oliver.stritzel@googlemail.com>
2021-07-23 09:53:14 +0200

    added reshape, sample, shuffle and normalize to data loading

--- load_lfw.py
+++ load_lfw.py
@@ -1,7 +1,8 @@
 import numpy as np
+import math
 
-from _lfw import *
-
+from _lfw import * # sklearn lfw code but without shuffling loaded images
+from sklearn.model_selection import train_test_split
 
 def _load_image_names():
     """
@@ -32,9 +33,16 @@ def _create_gender_target(imagenames, females):
             gender = np.append(gender, 0)
     return gender
 
-def load_lfw(min_faces=0):
+def _normalize(X_train, X_test):
+    # normalize mean
+    pixel_mean = np.mean(X_train, axis=0)
+    X_train -= pixel_mean
+    X_test -= pixel_mean
+    return X_train, X_test
+
+def load_lfw(min_faces=0, shuffle=True, resize=0.5):
     #from sklearn.datasets import fetch_lfw_people
-    lfw_people = fetch_lfw_people(min_faces_per_person=min_faces, resize=0.5, data_home="./", 
+    lfw_people = fetch_lfw_people(min_faces_per_person=min_faces, resize=resize, data_home="./", 
                                 download_if_missing=True)
 
     images = _load_image_names()
@@ -43,19 +51,40 @@ def load_lfw(min_faces=0):
 
     assert(target.shape == lfw_people.target.shape)
     
-    return lfw_people.data, target
+    data = lfw_people.data
+    data = data/255 # scale to [0,1]
+
+    N = data.shape[0]
+    H = math.ceil(124 * resize)
+    W = math.ceil(94 * resize)
+    data = data.reshape(N, H, W)
+
+    if shuffle:
+        ind = np.arange(target.shape[0])
+        np.random.RandomState(42).shuffle(ind)
+        data = data[ind]
+        target = target[ind]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        data, target, test_size=0.2, random_state=42, stratify=target)
+
+    X_train, X_test = _normalize(X_train, X_test)
+
+    return X_train, X_test, y_train, y_test
 
 if __name__ == '__main__':
-    #pass
+    pass
     
-    X,y = load_lfw()
+    """
+    # some debug stuff
+    x, xt, y, yt = load_lfw()
     print(y)
     print(y.sum())
     print(y.shape-y.sum())
 
     from PIL import Image
-    i = 0
-    print(X[i])
-    pil_image=Image.fromarray(X[i].reshape(62,47))
+    i = -3
+    print(x[i]*255)
+    pil_image=Image.fromarray(x[i]*255)
     pil_image.show()
-
+    """
